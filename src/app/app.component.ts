c12ede66d58e3d20f5fc9e609c7f1f7f0b0a206c
Nxaahh <noahh.montano@gmail.com>
2024-11-25 14:37:30 +0100

    Video 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { TasklistComponent } from './componentes/task/tasklist/tasklist.component';
+import { FormsModule } from '@angular/forms';
+import { TaskresumeComponent } from './componentes/task/taskresume/taskresume.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,TasklistComponent,RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, TasklistComponent, FormsModule, TaskresumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tutorial-app';
+  title = 'Curso Angular';
+  nombre = 'Noah Montaño'
+  imagenAleatorio:string=""
+
+  muestraImagen() {
+    let random:number = Math.trunc((Math.random()*1000)+100)
+    this.imagenAleatorio= "https://picsum.photos/200/300?random=" + random
+  }
+
 }
